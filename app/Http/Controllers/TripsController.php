5f7774ca830b98bd3b24c02ff15e174090cce4e7
Marwan Atef <marwanatef97@gmail.com>
2021-07-05 23:58:46 +0200

    get avialable seats

--- app/Http/Controllers/TripsController.php
+++ app/Http/Controllers/TripsController.php
@@ -54,3 +54,92 @@ class TripsController extends Controller
         }
     }
 
+    public function showAvailableSeats(Request $request)
+    {
+        $result = [];
+        $statusCode = 200;
+
+        $startStation = Station::firstWhere('name', $request->query('start'));
+        $endStation = Station::firstWhere('name', $request->query('end'));
+
+        $possibleTrips = CrossOverStations::select('trip_id', 'order_in_trip')
+                                ->where('station_id', '=', $startStation->id)
+                                ->get();
+
+        foreach ($possibleTrips as $tripStation) {
+            if ($this->isSuitableTrip($tripStation, $endStation)){
+                $availableSeats = $this->findAvailableSeats($startStation, $endStation, $tripStation);
+                if(!empty($availableSeats)){
+                    $trip = Trip::find($tripStation->trip_id);
+                    $resultPerTrip = [
+                        'trip' => [
+                            'id' => $trip->id,
+                            'name' => $trip->name
+                        ],
+                        'number_of_avaialable_seats' => count($availableSeats),
+                        'available_seats' => $availableSeats
+                    ];
+                    array_push($result, $resultPerTrip);
+                }
+            }
+        }
+
+        if (empty($result)){
+            $result = [
+                'message' => "No avaialable seats currently"
+            ];
+            $statusCode = 404;
+        }
+        return response(json_encode($result), $statusCode);
+    }
+
+    private function isSuitableTrip($tripStation, $endStation)
+    {
+        $tripIds = CrossOverStations::select('trip_id')
+                                    ->where([
+                                        ['station_id', '=', $endStation->id],
+                                        ['trip_id', '=', $tripStation->trip_id],
+                                        ['order_in_trip', '>', $tripStation->order_in_trip]
+                                    ])
+                                    ->get();
+        if (empty($tripIds->all()))
+            return false;
+        else return true;
+    }
+
+    private function findAvailableSeats($startStation, $endStation, $tripStation){
+        // find available seats at first station in user's trip
+        $availableSeats = CrossOverStations::where([
+                                ['station_id', $startStation->id],
+                                ['trip_id', $tripStation->trip_id],
+                            ])->first()
+                            ->availableSeats()
+                            ->pluck('seat_id');
+
+        // last station in user's trip
+        $endTripStation = CrossOverStations::firstWhere([
+                                ['station_id', '=', $endStation->id],
+                                ['trip_id', '=', $tripStation->trip_id],
+                            ]);
+
+        // get user's trip stations
+        $cross_over_stations = $this->getUserTripStations($tripStation, $endTripStation);;
+
+        // find common available seats through all users stations
+        foreach ($cross_over_stations as $station) {
+            $newAvailableSeats = $station->availableSeats()
+                                        ->pluck('seat_id');
+            $availableSeats = $availableSeats->intersect($newAvailableSeats);
+        }
+        
+        return array_values($availableSeats->all());
+    }
+
+    private function getUserTripStations($startTripStation, $endTripStation){
+        return CrossOverStations::where([
+            ['trip_id', '=', $startTripStation->trip_id],
+            ['order_in_trip', '>=' , $startTripStation->order_in_trip],
+            ['order_in_trip', '<=' , $endTripStation->order_in_trip]
+        ])->get();
+    }
+
