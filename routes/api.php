83cf627097d8b610b0b9cdcf6878c499f68be77c
Marwan Atef <marwanatef97@gmail.com>
2021-07-06 01:52:54 +0200

    add seats controller with get seats route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SeatsController;
 use App\Http\Controllers\TripsController;
 use App\Models\CrossOverStations;
 use App\Models\Seat;
@@ -25,58 +26,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users', function ()
-{
-    // $station1 = Station::create(['name' => "Cairo"]);
-    // $station2 = Station::create(['name' => "Alex"]);
-    // $station3 = Station::create(['name' => "Matroh"]);
-
-    // $station1 = Station::find(1);
-    // $station3 = Station::find(3);
-
-    // $trip = new Trip();
-    // $trip->startStation()->associate($station1);
-    // $trip->endStation()->associate($station3);
-    // $trip->name = $trip->startStation->name.'-'.$trip->endStation->name;
-    // $trip->save();
-
-    // $station1 = Station::create(['name' => "Aswan"]);
-    // $station2 = Station::create(['name' => "Luxor"]);
-    // $station = Station::find(2);
-
-    // $trip = Trip::find(1);
-    // $trip->crossOverStations()->attach($station, ['order_in_trip' => 1]);
-
-    // return $trip->stations();
-
-    return CrossOverStations::all();
-
-    // return User::all(['name', 'email']);
-    // return DB::select('select name, email from users');
-});
-
 Route::post('/trips', [TripsController::class, 'add']);
 
-Route::get('/trips', [TripsController::class, 'showAvailableSeats']);
+Route::get('/seats', [SeatsController::class, 'showAvailableSeats']);
 
 Route::post('/trips/seats', [TripsController::class, 'book']);
-
-Route::get('/test', function (Request $request){
-    // return Trip::find(2)->crossOverStations()->get(['name']);
-    // return Trip::find(2)->stations();
-    // CrossOverStations::truncate();
-    // Trip::truncate();
-
-    // $station1 = Station::create(['name' => "Cairo"]);
-    // $station2 = Station::create(['name' => "Alex"]);
-    // $station3 = Station::create(['name' => "Matroh"]);
-    //     $station1 = Station::create(['name' => "Aswan"]);
-    // $station2 = Station::create(['name' => "Luxor"]);
-
-    for ($i=0; $i < 12; $i++) { 
-        $seat = new Seat();
-        $seat->save();
-    }
-    
-    return "good";
-});
